f34bf4d2a4cefe5a02a11dc6db22468463da950f
Ben <benshiundu@gmail.com>
2020-09-05 00:08:56 +0200

    chore(Routing): moved routing to core, added material ui

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './core/routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingComponent } from './landing/landing.component';
 import { ListingsComponent } from './listings/listings.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-  MatButtonModule,
-  MatProgressSpinnerModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent,
     ListingsComponent
   ],
   imports: [
--- fe/src/app/core/routing/app-routing.module.ts
+++ fe/src/app/core/routing/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from '../../app.component'
+import { ListingsComponent } from '../../listings/listings.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full'},
+  { path: '', component: AppComponent },
+  { path: 'listings', component: ListingsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

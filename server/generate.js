71cb59656bbf1e45bfea64125a94dfb033fc5dbc
Ben <benshiundu@gmail.com>
2020-09-05 00:01:39 +0200

    fix(Server): mock server

---
+++ server/generate.js
@@ -0,0 +1,14 @@
+var faker = require('faker');
+
+var database = { items: []};
+
+for (var i = 1; i<= 300; i++) {
+  database.items.push({
+    id: i,
+    name: faker.commerce.productName(),
+    description: faker.lorem.sentences(),
+    imageUrl: "https://source.unsplash.com/1600x900/?product",
+  });
+}
+
+console.log(JSON.stringify(database));

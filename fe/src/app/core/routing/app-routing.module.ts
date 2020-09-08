9b16066584d8048a6f48980648665c784ef481a0
Ben <benshiundu@gmail.com>
2020-09-08 17:03:26 +0200

    fix(Routing): added new routes

--- fe/src/app/core/routing/app-routing.module.ts
+++ fe/src/app/core/routing/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from '../../app.component'
 import { ListingsComponent } from '../../listings/listings.component'
+import { ItemComponent } from '../../listings/item/item.component'
+import { AddItemComponent } from '../../listings/add-item/add-item.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full'},
   { path: '', component: AppComponent },
+  { path: 'listings/new', component: AddItemComponent },
+  { path: 'listings/:id', component: ItemComponent },
   { path: 'listings', component: ListingsComponent },
 ];
 
